5ae2254fa7c71ed90d7bb6980f09becf6705a2f4
Gustavo Lima <limasud@gmail.com>
2022-04-18 00:33:58 -0400

    Se modifica la validación del form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function validateForm() {
-  let x = document.forms["myForm"]["fName", "fSubject", "fMessage"].value;
+  let x = document.forms["myForm"]["fName", "fMessage"].value;
   if (x == "") {
     alert("Lo sentimos! Para enviar el formulario debe llenar los campos nombre, asunto y mensaje. Vuelva a intentarlo.");
     return false;
