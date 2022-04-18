68fc27da87aa67a299461650a2bc98d393ae9fcc
Gustavo Lima <limasud@gmail.com>
2022-04-17 23:51:39 -0400

    Modificaciones finales y se agrega funcionalidades al form y cards

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function validateForm() {
+  let x = document.forms["myForm"]["fName", "fSubject", "fMessage"].value;
+  if (x == "") {
+    alert("Lo sentimos! Para enviar el formulario debe llenar los campos nombre, asunto y mensaje. Vuelva a intentarlo.");
+    return false;
+  } else {
+    alert("Su mensaje ha sido enviado exitosamente!");
+    return true;
+  }
+}
+
